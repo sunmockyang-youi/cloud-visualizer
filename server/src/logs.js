2b13b87e0c31dcd66763f8b1429f725b893dcc9e
Alex Pilon <apilo088@gmail.com>
2017-09-27 16:47:34 -0400

    make time windows configurable

--- server/src/logs.js
+++ server/src/logs.js
@@ -1,18 +1,21 @@
 "use strict";
 
 const Promise = require("bluebird");
+const env = require("require-env");
 const AWS = require("aws-sdk");
 AWS.config.setPromisesDependency(Promise);
 const apiVersion = "2014-03-28";
 const startFromHead = true;
 
-// hardcoded log groups/streams for filmstruck us-east-1
-const eastLogGroup = "fs-v3-0-v802e1-NodeProxyECSCluster";
-const eastLogStream = "youi/nodeproxy/8c31bb87-5807-4d75-b8af-f57ac8412cce";
+const eastLogGroup = env.require("EAST_LOG_GROUP");
+const eastLogStream = env.require("EAST_LOG_STREAM");
 
-// hardcoded log groups/streams for filmstruck us-west-2
-const westLogGroup = "fs-v3-0-v802w2-NodeProxyECSCluster";
-const westLogStream = "youi/nodeproxy/c3c37d5c-0a07-4074-81fd-adbbe8e5fb25";
+const westLogGroup = env.require("WEST_LOG_GROUP");
+const westLogStream = env.require("WEST_LOG_STREAM");
+
+const window = process.env.DATA_WINDOW || 2;
+const pruneInterval = process.env.PRUNE_INTERVAL || 5000;
+const fetchInterval = process.env.FETCH_INTERVAL || 5000;
 
 function minutesAgo(n) {
   return Date.now() - n * 60 * 1000;
@@ -42,7 +45,7 @@ function getLogs(region) {
   if (!!region.nextToken) {
     region.params.nextToken = region.nextToken;
   }
-  region.params.startTime = minutesAgo(2);
+  region.params.startTime = minutesAgo(window);
   return region.cloudWatchLogs.getLogEvents(region.params).promise();
 }
 
@@ -52,7 +55,7 @@ function parseLog(log) {
     if (log.event === "clientMessage") {
       let clientMsg = JSON.parse(log.msg);
       let event = clientMsg.event || "";
-      if (["selection", "focus", "key"].indexOf(event) >= 0) {
+      if (["selection", "focus", "key", "ping"].indexOf(event) >= 0) {
         evts.push({ id: log.id, time: log.time, event });
       }
     }
@@ -81,7 +84,7 @@ function poll(region) {
     .then(r => r.events)
     .tap(parseEvents)
     .then(events => {
-      if (events.length === 0) return Promise.delay(5000);
+      if (events.length === 0) return Promise.delay(fetchInterval);
     })
     .then(() => poll(region));
 }
@@ -90,10 +93,11 @@ poll(regions["us-east-1"]).catch(e => console.error(e));
 poll(regions["us-west-2"]).catch(e => console.error(e));
 
 function pruneData() {
+  let cutoff = minutesAgo(window);
   // prune connection id list
   let nextConns = {};
   for (let id in conns) {
-    if (conns[id] >= minutesAgo(2)) {
+    if (conns[id] >= cutoff) {
       nextConns[id] = conns[id];
     }
   }
@@ -102,20 +106,20 @@ function pruneData() {
   // prune event data
   let newEvts = [];
   for (let evt of evts) {
-    if (evt.time >= minutesAgo(2)) {
+    if (evt.time >= cutoff) {
       newEvts.push(evt);
     }
   }
   evts = newEvts;
 }
-setInterval(pruneData, 5000);
+setInterval(pruneData, pruneInterval);
 
 function connections() {
   return Object.keys(conns);
 }
 
 function events() {
-  return evts;
+  return evts.sort((a, b) => a.time - b.time);
 }
 
 module.exports = {
