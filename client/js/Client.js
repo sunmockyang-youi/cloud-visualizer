7c706e10cd295be605c7f2df719fb000e95f9f18
Alex Pilon <apilo088@gmail.com>
2017-09-27 17:31:43 -0400

    git checkin temp code for network requests

--- client/js/Client.js
+++ client/js/Client.js
@@ -1,11 +1,32 @@
 class Client {
-	constructor(onUserLoggedIn, onUserLoggedOut) {
-		this.onUserLoggedIn = onUserLoggedIn;
-		this.onUserLoggedOut = onUserLoggedOut;
+  constructor(onUserLoggedIn, onUserLoggedOut) {
+    this.onUserLoggedIn = onUserLoggedIn;
+    this.onUserLoggedOut = onUserLoggedOut;
 
-		this.fetch();
-	}
+    const req = () => {
+      let headers = {
+        "Access-Control-Allow-Origin": "*"
+      };
+      let mode = "no-cors";
+      let host = "http://ec2-52-91-154-219.compute-1.amazonaws.com";
+      let options = { headers, mode };
+      fetch(host + "/api/connections", options)
+        .then(this.connectionsHandler.bind(this))
+        .catch(this.errorHandler.bind(this));
 
-	fetch() {
-	}
+      fetch(host + "/api/events", options)
+        .then(this.eventsHandler.bind(this))
+        .catch(this.errorHandler);
+    };
+    req();
+
+    setInterval(req, 60 * 1000);
+  }
+  connectionsHandler(response) {
+    console.log(response);
+  }
+  eventsHandler(response) {}
+  errorHandler(e) {
+    console.error(e);
+  }
 }
